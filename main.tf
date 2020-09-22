89f086298a61a8e581d888edbb51b58c3ebb0e91
sekharreddy0463 <53187022+sekharreddy0463@users.noreply.github.com>
2020-09-22 17:17:30 +0530

    Update main.tf
    hh

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Authentication
 provider "aws" {
   region     = "eu-west-2"
-  access_key = "AKIAT2MSWNDRWMRREREI"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 data "aws_availability_zones" "available" {}
 
